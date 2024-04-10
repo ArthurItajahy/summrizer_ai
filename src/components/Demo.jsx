29759e276a6030d48b1eb1941dfaadf7446ad054
Arthur Itajahy <canalp3rry@gmail.com>
2024-04-10 09:05:14 -0300

    feat: finish the Demo without the logic part or AI.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import {useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../../public/assets';
+
 
 const Demo = () => {
+
+    const [article, setArticle ] = useState({
+        url: '',
+        summary: "",
+    });
+
+    const handleSubmit = async (e) => {
+        
+    }
+
+
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+        {/* Search */}
+        <div className='flex flex-col w-full gap-2'>
+
+            <form className=' relative flex
+            justify-center items-center'
+            onSubmit={handleSubmit}
+
+            >
+                <img src={linkIcon} alt="link_icon" 
+                className='absolute left-0 my-2 ml-3 w-5'/>
+                <input 
+                    type="url"
+                    placeholder='Enter a url'
+                    valuer={article.url}
+                    onChange={(e) => {setArticle({...article, url: e.target.value})}}
+                    required
+                    className='url_input peer'
+                />
+                <button
+                    type='submit'
+                    className='submit_btn 
+                    peer-focus:border-emerald-200
+                    peer-focus:text-emerald-200'
+                >
+                    ↲
+
+
+                </button>
+            </form>
+            {/* Browser URL History  */}
+
+        </div>
+        {/*  Display Results */}
+
+    </section>
   )
 }
 
